99496cbef535e84eb7e971d97e7a2a632b5c43c0
Raghavendra0088 <raghavendrag0088@gmail.com>
2026-04-24 15:39:25 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,4 @@
-Calculator.java
-    public class Calculator {
+public class Calculator {
 
     public int add(int a, int b) {
         return a + b;
@@ -26,5 +25,17 @@ Calculator.java
 
     public int max(int a, int b) {
         return (a > b) ? a : b;
+    }
+
+   
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Add: " + calc.add(5, 3));
+        System.out.println("Subtract: " + calc.subtract(5, 3));
+        System.out.println("Multiply: " + calc.multiply(5, 3));
+        System.out.println("Divide: " + calc.divide(5, 3));
+        System.out.println("Square: " + calc.square(5));
+        System.out.println("Max: " + calc.max(5, 3));
     }
 }
