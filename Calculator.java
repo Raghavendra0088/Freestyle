1784175ef4b3e6b1cff7f8ef979c8f02ddb8a52f
Raghavendra0088 <raghavendrag0088@gmail.com>
2026-04-24 15:31:11 +0530

    Add basic calculator functionality
    Implement basic calculator operations including addition, subtraction, multiplication, division, squaring, and finding the maximum of two numbers.

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+
+    public int square(int a) {
+        return a * a;
+    }
+
+    public int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+}
